c3f31a36ab4c60077e49b7314a49dba5f1a3ba9c
Sergio-Ingegneri <sergio.santos@sptech.school>
2025-05-14 22:25:25 -0300

    implementando API

---
+++ Banco de dados/Script.sql
@@ -0,0 +1,10 @@
+-- BD BodyPedia
+
+-- criando as tabelas
+
+create table usuario(
+			 id int not null primary key auto_increment,
+             email varchar(100),
+             nome varchar(100,
+             senha varchar(20)
+);
