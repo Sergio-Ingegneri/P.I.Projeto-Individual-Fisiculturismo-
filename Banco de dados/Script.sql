71e48eaccb190aa81336a982420e308d2a74e7c5
Sergio-Ingegneri <sergio.santos@sptech.school>
2025-05-17 18:45:09 -0300

    Login e Cadastro integrados com a API

--- Banco de dados/Script.sql
+++ Banco de dados/Script.sql
@@ -5,6 +5,6 @@
 create table usuario(
 			 id int not null primary key auto_increment,
              email varchar(100),
-             nome varchar(100,
+             nome varchar(100),
              senha varchar(20)
 );
