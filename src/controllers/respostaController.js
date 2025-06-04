dc8f6e52f4d6140d9c728bd91528bcccea93f23a
Sergio-Ingegneri <sergio.santos@sptech.school>
2025-06-04 10:59:00 -0300

    Código revisado sem comentários

--- src/controllers/respostaController.js
+++ src/controllers/respostaController.js
@@ -4,25 +4,30 @@ function cadastrar(req, res) {
   var idUsuario = req.body.idUsuario;
   var respostas = req.body.respostas;
 
-  if (!idUsuario || !Array.isArray(respostas) || respostas.length == 0) {
-    res.status(400).send("Dados inválidos!");
-  } else {
-    var insercoes = 0;
+  if (!idUsuario) {
+    return res.status(400).send("ID do usuário não enviado!");
+  }
 
-    for (var i = 0; i < respostas.length; i++) {
-      var r = respostas[i];
+  if (!respostas || !respostas.length) {
+    return res.status(400).send("Respostas inválidas ou vazias!");
+  }
 
-      respostaModel.cadastrar(idUsuario, r.idPergunta, r.resposta, r.correta, r.acertou)
-        .then(function () {
-          insercoes++;
-          if (insercoes == respostas.length) {
-            res.status(200).send("Respostas salvas com sucesso!");
-          }
-        })
-        .catch(function (erro) {
-          res.status(500).json(erro.sqlMessage);
-        });
-    }
+  var insercoes = 0;
+
+  for (var i = 0; i < respostas.length; i++) {
+    var r = respostas[i];
+
+    respostaModel.cadastrar(idUsuario, r.idPergunta, r.resposta, r.correta, r.acertou)
+      .then(function () {
+        insercoes++;
+
+        if (insercoes == respostas.length) {
+          res.status(200).send("Respostas salvas com sucesso!");
+        }
+      })
+      .catch(function (erro) {
+        res.status(500).json(erro.sqlMessage);
+      });
   }
 }
 
